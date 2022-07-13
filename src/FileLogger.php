619bd1d619e1f2b8e2d6b8d05ae6b81a86c181cf
Sergey Popov <popserg5555@gmail.com>
2022-07-13 20:31:21 +0300

    change added in FileLogger

--- src/FileLogger.php
+++ src/FileLogger.php
@@ -25,46 +25,46 @@ class FileLogger implements LoggerInterface
     public function log($level, $message, array $context = []): void
     {
         $date = date('Y-m-d G:i:s');
-        $context = print_r($message, true);
-        fwrite($this->fileHandle, "$date | $level | $context \n");
+        $context = print_r($context, true);
+        fwrite($this->fileHandle, "$date | $level | $message | $context \n");
     }
 
-    public function emergency($message, array $context = array()): void
+    public function emergency($message, array $context = []): void
     {
         $this->log(LogLevel::EMERGENCY, $message, $context);
     }
 
-    public function alert($message, array $context = array()): void
+    public function alert($message, array $context = []): void
     {
         $this->log(LogLevel::ALERT, $message, $context);
     }
 
-    public function critical($message, array $context = array()): void
+    public function critical($message, array $context = []): void
     {
         $this->log(LogLevel::CRITICAL, $message, $context);
     }
 
-    public function error($message, array $context = array()): void
+    public function error($message, array $context = []): void
     {
         $this->log(LogLevel::ERROR, $message, $context);
     }
 
-    public function warning($message, array $context = array()): void
+    public function warning($message, array $context = []): void
     {
         $this->log(LogLevel::WARNING, $message, $context);
     }
 
-    public function notice($message, array $context = array()): void
+    public function notice($message, array $context = []): void
     {
         $this->log(LogLevel::NOTICE, $message, $context);
     }
 
-    public function info($message, array $context = array()): void
+    public function info($message, array $context = []): void
     {
         $this->log(LogLevel::INFO, $message, $context);
     }
 
-    public function debug($message, array $context = array()): void
+    public function debug($message, array $context = []): void
     {
         $this->log(LogLevel::DEBUG, $message, $context);
     }
